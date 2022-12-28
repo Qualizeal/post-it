3dda4091312dbd370f9464aa9a37d4bd3d61c7e6
arjun <arjun.rajeev@qualizeal.com>
2022-12-28 18:28:03 +0530

    #AC-35 Fix broken tests

--- src/test/java/com/qualizeal/parser/ParserTest.java
+++ src/test/java/com/qualizeal/parser/ParserTest.java
@@ -14,14 +14,14 @@ import java.util.stream.Collectors;
 public class ParserTest {
 	@Test
 	public void shouldBeAbleToParseTheXrayConfigFile() throws FileNotFoundException {
-		ToolConfig xrayConfig = new ConfigParser().parse("/Users/arjun/Workspace/utaf/post-it/src/test/resources/xrayConfig.json", ToolConfig.class);
+		ToolConfig xrayConfig = new ConfigParser().parse("src/test/resources/xrayConfig.json", ToolConfig.class);
 		Assert.assertEquals("Automation identifier is not correct", "customfield_10048", xrayConfig.getAutomationReferenceIdentifier());
 		Assert.assertEquals("Automation credential is not correct", AuthType.APIKEY, xrayConfig.getCredential().getAuthType());
 	}
 
 	@Test
 	public void shouldBeAbleToParseTheResultConfigFile() throws FileNotFoundException {
-		TestConfig[] testResults = new ConfigParser().parse("/Users/arjun/Workspace/utaf/post-it/src/test/resources/testResults.json", TestConfig[].class);
+		TestConfig[] testResults = new ConfigParser().parse("src/test/resources/testResults.json", TestConfig[].class);
 		List<TestConfig> results = Arrays.stream(testResults).collect(Collectors.toList());
 		Assert.assertEquals("All tests are not retrieved", 4, results.size());
 		Assert.assertEquals("Results are not correct", "Pass", results.get(0).getStatus());
